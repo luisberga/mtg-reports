1b6c2c1e667338c037d78d0e37656890a196791f
luisberga <luisfelipebergamin@gmail.com>
2025-06-09 23:10:59 -0300

    add cors

---
+++ internal/adapters/handlers/apihandler/cors.go
@@ -0,0 +1,19 @@
+package apihandler
+
+import "net/http"
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+		w.Header().Set("Access-Control-Max-Age", "86400")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
